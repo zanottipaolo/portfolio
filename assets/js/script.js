477726eb477d32d75e16aa830d202e50ef4d0f8e
Paolo Zanotti <60442209+zanottipaolo@users.noreply.github.com>
2022-04-11 22:17:04 +0200

    ✏️ Add Navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,28 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 let typed = new Typed('#typed', {
     stringsElement: '#typed-strings',
     typeSpeed: 70,
-    backSpeed: 45,
+    backSpeed: 70,
     backDelay: 2000,
     loop: true,
     smartBackspace: true,
 });
+
+// Change Navbar style when scrolling
+window.onscroll = function (e) {
+    let div = document.getElementById('navbar');
+
+    if(window.scrollY==0){
+        div.classList.remove("bg-base-100");
+        div.classList.add("bg-transparent");
+
+        div.classList.add("py-6");
+        div.classList.remove("py-4");
+    }
+    else{
+        div.classList.add("bg-base-100");
+        div.classList.remove("bg-transparent");
+
+        div.classList.add("py-4");
+        div.classList.remove("py-6");
+    }
+} 
