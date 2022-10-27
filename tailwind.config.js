474ec70455382ef9f76dfe52b02b8b16e38ce5da
Paolo Zanotti <zanotti.paolo2001@gmail.com>
2022-10-27 02:00:12 +0200

    💄 Add new themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,41 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [{
+      lightgreen: {
+        ...require("daisyui/src/colors/themes")["[data-theme=cmyk]"],
+        primary: "#1FAB89",
+        secondary: "#62D2A2",
+        accent: "#9DF3C4",
+        neutral: "#D7FBE8",
+        "fontFamily": "Chalkboard, comic sans ms",
+      },
+      darkgreen: {
+        ...require("daisyui/src/colors/themes")["[data-theme=dracula]"],
+        primary: "#1FAB89",
+        secondary: "#62D2A2",
+        accent: "#9DF3C4",
+        neutral: "#D7FBE8",
+        "fontFamily": "Chalkboard, comic sans ms",
+      },
+      lightred: {
+        ...require("daisyui/src/colors/themes")["[data-theme=cmyk]"],
+        primary: "#F67280",
+        secondary: "#C06C84",
+        accent: "#6C5B7B",
+        neutral: "#355C7D",
+        "fontFamily": "Chalkboard, comic sans ms",
+      },
+      darkred: {
+        ...require("daisyui/src/colors/themes")["[data-theme=dracula]"],
+        primary: "#F67280",
+        secondary: "#C06C84",
+        accent: "#6C5B7B",
+        neutral: "#355C7D",
+        "fontFamily": "Chalkboard, comic sans ms",
+      },
+    }, 
+    "cupcake", "bumblebee", "emerald", "cyberpunk", "valentine", "halloween", "aqua", "pastel", "wireframe", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "night", "winter"],
+  }
 }
