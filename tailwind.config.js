123d57f85b6469d7bedf13d6f22db2398a9a734e
Paolo Zanotti <zanotti.paolo2001@gmail.com>
2022-10-27 12:37:41 +0200

    💄 Change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,40 +6,40 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [{
+    themes: [
+      {
       lightgreen: {
         ...require("daisyui/src/colors/themes")["[data-theme=cmyk]"],
+        "fontFamily": 'Chalkboard,comic sans ms',
         primary: "#1FAB89",
         secondary: "#62D2A2",
         accent: "#9DF3C4",
         neutral: "#D7FBE8",
-        "fontFamily": 'Chalkboard,comic sans ms,"sanssecondaryerif"',
       },
       darkgreen: {
         ...require("daisyui/src/colors/themes")["[data-theme=dracula]"],
+        "fontFamily": 'Chalkboard,comic sans ms',
         primary: "#1FAB89",
         secondary: "#62D2A2",
         accent: "#9DF3C4",
         neutral: "#D7FBE8",
-        "fontFamily": 'Chalkboard,comic sans ms,"sanssecondaryerif"',
       },
       lightred: {
         ...require("daisyui/src/colors/themes")["[data-theme=cmyk]"],
+        "fontFamily": 'Chalkboard,comic sans ms',
         primary: "#F67280",
         secondary: "#C06C84",
         accent: "#6C5B7B",
         neutral: "#355C7D",
-        "fontFamily": 'Chalkboard,comic sans ms,"sanssecondaryerif"',
       },
       darkred: {
         ...require("daisyui/src/colors/themes")["[data-theme=dracula]"],
+        "fontFamily": 'Chalkboard,comic sans ms',
         primary: "#F67280",
         secondary: "#C06C84",
         accent: "#6C5B7B",
         neutral: "#355C7D",
-        "fontFamily": 'Chalkboard,comic sans ms,"sanssecondaryerif"',
-      },
-    }, 
+      }}, 
     "cupcake", "bumblebee", "emerald", "cyberpunk", "valentine", "halloween", "aqua", "pastel", "wireframe", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "night", "winter"],
   }
 }
