3dab55611f073db8ee13b5fcd2191213910c4ab6
Paolo Zanotti <zanotti.paolo2001@gmail.com>
2022-10-27 18:51:20 +0200

    💄 Update style

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
       {
       lightgreen: {
         ...require("daisyui/src/colors/themes")["[data-theme=cmyk]"],
-        "fontFamily": 'Chalkboard,comic sans ms,' + defaultTheme.fontFamily.sans,
+        "fontFamily": 'Balsamiq San,Chalkboard,comic sans ms,' + defaultTheme.fontFamily.sans,
         primary: "#1FAB89",
         secondary: "#62D2A2",
         accent: "#9DF3C4",
@@ -21,7 +21,7 @@ module.exports = {
       },
       darkgreen: {
         ...require("daisyui/src/colors/themes")["[data-theme=dracula]"],
-        "fontFamily": 'Chalkboard,comic sans ms,' + defaultTheme.fontFamily.sans,
+        "fontFamily": 'Balsamiq San,Chalkboard,comic sans ms,' + defaultTheme.fontFamily.sans,
         primary: "#1FAB89",
         secondary: "#62D2A2",
         accent: "#9DF3C4",
@@ -29,7 +29,7 @@ module.exports = {
       },
       lightred: {
         ...require("daisyui/src/colors/themes")["[data-theme=cmyk]"],
-        "fontFamily": 'Chalkboard,comic sans ms,' + defaultTheme.fontFamily.sans,
+        "fontFamily": 'Balsamiq San,Chalkboard,comic sans ms,' + defaultTheme.fontFamily.sans,
         primary: "#F67280",
         secondary: "#C06C84",
         accent: "#6C5B7B",
@@ -37,7 +37,7 @@ module.exports = {
       },
       darkred: {
         ...require("daisyui/src/colors/themes")["[data-theme=dracula]"],
-        "fontFamily": 'Chalkboard,comic sans ms,' + defaultTheme.fontFamily.sans,
+        "fontFamily": 'Balsamiq San,Chalkboard,comic sans ms,' + defaultTheme.fontFamily.sans,
         primary: "#F67280",
         secondary: "#C06C84",
         accent: "#6C5B7B",
